b4a7fd1bf5150f80d7e73b8e0c8495984a0aa8cc
Ali Sadikin <alisadikinmaai@gmail.com>
2025-09-06 22:09:42 +0700

    remove unused files

--- clear_cache.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "C:\xampp\htdocs\ALI-PORTO"
-php artisan cache:clear
-php artisan config:clear
-php artisan view:clear
-php artisan route:clear
-echo Cache cleared successfully!
-pause
